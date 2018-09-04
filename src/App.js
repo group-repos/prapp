e5b8c6ea0ac76a6e4737d92aeeb7468bf2e6bdda
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-09-04 15:47:01 -0600

    another test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Our Group Project</h1>
-          <h1 className="App-title">You'll shoot your eye out, kid</h1>
+          <h1 className="App-title">SHUT UP RICHARD!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
