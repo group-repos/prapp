f69e4b845ba50d5b9abb4c5bc2f420501b07aa53
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-09-04 14:29:32 -0600

    initial commit

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Our Group Project</h1>
+          <h1 className="App-title">You'll shoot your eye out, kid</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
