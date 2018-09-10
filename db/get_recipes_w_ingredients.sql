f276f6c8804eb42e850d802518bf654db558f050
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-09-10 09:32:36 -0600

    Added functionality for recipe browser

--- db/get_recipes_w_ingredients.sql
+++ db/get_recipes_w_ingredients.sql
@@ -1,5 +1,5 @@
 SELECT recipes.r_id, recipes.r_name
-    , json_agg (json_build_object('ingredient',ingredients.ingredient, 'quantity', ingredients.quantity, 'unit',ingredients.unit)) AS ingredients
+    , json_agg (json_build_object('id', ingredients.i_id, 'ingredient',ingredients.ingredient, 'quantity', ingredients.quantity, 'unit',ingredients.unit)) AS ingredients
 FROM   recipes
 JOIN   ingredients ON ingredients.r_id = recipes.r_id
 GROUP  BY recipes.r_name, recipes.r_id
--- db/get_recipes_w_steps.sql
+++ db/get_recipes_w_steps.sql
@@ -1,5 +1,5 @@
 SELECT recipes.r_id, recipes.r_name
-    , json_agg (json_build_object('step', steps.step, 'description', steps.description)) AS steps
+    , json_agg (json_build_object('id', steps.s_id, 'step', steps.step, 'description', steps.description)) AS steps
 FROM   recipes
 JOIN steps on steps.r_id = recipes.r_id
 GROUP  BY recipes.r_name, recipes.r_id
