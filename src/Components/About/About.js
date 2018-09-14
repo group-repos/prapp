1dc1607b97d1e51e0dd73e3aaac4e98c910a9093
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-09-13 20:11:17 -0600

    Content for about page is present. Made logo clickable to home page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './About.css';
+
+import spencer from '../../images/Spencer.jpg';
+import dylan from '../../images/Dylan.jpg';
+import timboSlice from '../../images/TimboSlice.jpg';
+
 
 export default function About() {
     return(
         <div>
             <h2>About</h2>
+            <p>In our opinion, meal tracking apps in today's market are a super-adequacy. Too many apps allow you to add a recipe, but none of them allow you to plan your week. Here at Prapp, our goal is to empower goal-oriented people to easily plan their food each week by choosing a recipe and having an already populated shopping list when their planning is complete.</p>
+            <h3>Meet the developers</h3>
+            <div className='photos'>
+                <div>
+                    <img src={dylan} alt='' />
+                </div>
+                <div>
+                    <img src={spencer} alt='' />
+                </div>
+                <div>
+                    <img src={timboSlice} alt='' />
+                </div>
+            </div>
         </div>
     )
 }
