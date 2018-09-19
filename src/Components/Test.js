3a717fc66c350f9182dc17a9fd0a4c50090b3db7
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-09-18 18:30:21 -0600

    Added Shopping List

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -5,7 +5,8 @@ import AddRecipe from './Modals/AddRecipe/AddRecipe';
 import AddIngredients from './Modals/AddIngredients/AddIngredients';
 import AddSteps from './Modals/AddSteps/AddSteps';
 import Calendar from './Modals/Calendar/Calendar';
-import BrowseRecipes from './BrowseRecipes/BrowseRecipes'
+import BrowseRecipes from './BrowseRecipes/BrowseRecipes';
+import ShoppingList from './ShoppingList/ShoppingList';
 
 class Test extends Component {
     state = {
@@ -15,6 +16,7 @@ class Test extends Component {
     render () {
         return (
             <div style={{marginTop: '50px'}}>
+                <ShoppingList/>
                 <Calendar />
                 <BrowseRecipes />
                 {/* <AddRecipe /> */}
